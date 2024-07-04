637e24f00a15585f407661a06b22a800f3bf6d70
philylli <philylli@tencent.com>
2024-07-04 23:47:20 +0800

    最长回文串

--- main.go
+++ main.go
@@ -1,60 +1,25 @@
 package main
 
-func main() {
-
-}
-
-type MyQueue struct {
-	inStack  []int
-	outStack []int
-}
-
-func Constructor() MyQueue {
-	return MyQueue{}
-}
-
-func (this *MyQueue) Push(x int) {
-	// 将元素加入inStack
-	this.inStack = append(this.inStack, x)
-}
-
-func (this *MyQueue) Pop() int {
-	popNum := this.Peek()
-	this.outStack = this.outStack[:len(this.outStack)-1]
-	return popNum
-}
-
-func (this *MyQueue) Peek() int {
-	// 判断队列是否为空
-	if this.Empty() {
-		return -1
+func longestPalindrome(s string) int {
+	if len(s) == 0 {
+		return 0
 	}
 
-	if len(this.outStack) == 0 {
-		// 元素从inStack移出，然后移入outStack
-		length := len(this.inStack)
-		for i := length - 1; i >= 0; i-- {
-			this.outStack = append(this.outStack, this.inStack[i])
+	counter := make(map[byte]int, 0)
+	for i := 0; i < len(s); i++ {
+		counter[s[i]]++
+	}
+
+	res := 0
+	odd := 0
+	for _, count := range counter {
+		if count%2 == 0 {
+			res += count
+		} else {
+			res += count - 1
+			odd = 1
 		}
-		// 移出完毕后置空
-		this.inStack = []int{}
 	}
 
-	return this.outStack[len(this.outStack)-1]
+	return res + odd
 }
-
-func (this *MyQueue) Empty() bool {
-	if len(this.inStack) == 0 && len(this.outStack) == 0 {
-		return true
-	}
-	return false
-}
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(x);
- * param_2 := obj.Pop();
- * param_3 := obj.Peek();
- * param_4 := obj.Empty();
- */
