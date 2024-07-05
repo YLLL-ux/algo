0f08c5ad389d79a56b06b2fb4eceb46cb515849e
philylli <philylli@tencent.com>
2024-07-05 21:30:45 +0800

    判断子序列

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
-	// learning algo
+func isSubsequence(s string, t string) bool {
+	x := len(s)
+	y := len(t)
+	if x == 0 && y == 0 {
+		return true
+	}
+
+	i := 0
+	j := 0
+	for i < x && j < y {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+
+	return i == x
 }
