f677a2974bd0b186dab3d4be294d4853c73ad928
philylli <philylli@tencent.com>
2024-07-05 22:33:59 +0800

    链表的中间结点

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
-	// learning algo
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	fast := head
+	slow := head
+
+	for fast != nil || fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
 }
