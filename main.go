7b2cc1911f19e1ce53c84f198699d4794aa9e975
phil <phil@github.com>
2024-07-09 08:58:12 +0800

    相交链表

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
-	// learning algo
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	A := headA
+	B := headB
+	for A != B { // A指针与B指针重合位置，即为返回值
+		if A != nil {
+			A = A.Next
+		} else { // A遍历完后，A指针指向headB
+			A = headB
+		}
+
+		if B != nil {
+			B = B.Next
+		} else { // B遍历完后，B指针指向headA
+			B = headA
+		}
+	}
+
+	return A
 }
